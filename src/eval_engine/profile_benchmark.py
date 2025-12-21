17869a270428640f9378fb8ba9153b84b7e76822
zhangruipeng <zhangruipeng@gmail.com>
2025-12-22 02:32:52 +0800

    fix

--- src/eval_engine/profile_benchmark.py
+++ src/eval_engine/profile_benchmark.py
@@ -6,8 +6,8 @@ from src.profiling.profiling import get_profile_data
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, default="devnagari")
-    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--dataset", type=str, default="ccfraud")
+    parser.add_argument("--batch_size", type=int, default=256)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--num_cpus", type=int, default=10)
@@ -41,14 +41,14 @@ if __name__ == '__main__':
     # 2888.335 * 4 / 10000 = 1.155334
 
 
-    res = 5693.425184011459  * 4 / 10000
-    print(res)
-    # T2 * p = t2 * C * (log_eta (R/U_init) + 1)
-    res2 = 11480.500545024872 * 4 / (2000 * 5)
-    print(res2)
+    # res = 5693.425184011459  * 4 / 10000
+    # print(res)
+    # # T2 * p = t2 * C * (log_eta (R/U_init) + 1)
+    # res2 = 11480.500545024872 * 4 / (2000 * 5)
+    # print(res2)
     #
-    for i in range(1,   17174+1):
-        kv = get_profile_data(dataset="devnagari")
+    for i in range(1,   14219+1):
+        kv = get_profile_data(dataset="ccfraud")
         t1 = kv["t1"]
         t2 = kv["t2"]
         sh = BudgetAwareCoordinatorSH(args=args, budget=i, explore_profile_time=t1, exploit_profile_time=t2, only_one_phase=False)
