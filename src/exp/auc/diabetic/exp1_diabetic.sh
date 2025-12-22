c32f9350e876cb65509d24cd32b4b74ffe45ba69
zhangruipeng <zhangruipeng@gmail.com>
2025-12-22 12:12:31 +0800

    fix

--- src/exp/auc/diabetic/exp1_diabetic.sh
+++ src/exp/auc/diabetic/exp1_diabetic.sh
@@ -5,7 +5,7 @@ echo "===================================="
 echo "Dataset: diabetic"
 
 #files=("exp2_asha.py" "exp2_bohb.py" "exp2_hyperband.py" "exp1_2phase.py")
-files=("../exp1_2phase.py")
+files=("../exp2_asha.py")
 echo "===================================="
 echo "The following scripts will be executed in order:"
 for f in "${files[@]}"; do
@@ -18,7 +18,7 @@ for f in "${files[@]}"; do
     echo "------------------------------------"
     echo "Running: $f"
 
-    python "$f" --dataset=diabetic --batch_size=128 --seed=42 --num_samples=10000 --max_epochs=16
+    python "$f" --dataset=diabetic --batch_size=128 --seed=42 --num_samples=10000 --max_epochs=16 --device_ids=2,3 --gpu_ids=2,3,2,3
     status=$?
 
     if [ $status -ne 0 ]; then
